651db7a21675860a42d37c2700238d90a8fc681d
anthonycruz <anthonygabrieloliveiracruz@gmail.com>
2024-11-18 17:18:06 -0400

    Back end completo porem sem a implementação com o Front

---
+++ login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String passoword;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassoword() {
+        return passoword;
+    }
+
+    public void setPassoword(String passoword) {
+        this.passoword = passoword;
+    }
+}
